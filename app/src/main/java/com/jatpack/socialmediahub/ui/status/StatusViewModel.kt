d185a9a7b8582bcd56631d102e0480a5498ec4b8
rakesh singh <rakesh.singh7476@gmail.com>
2022-08-25 10:47:16 +0530

    commite structure

---
+++ app/src/main/java/com/jatpack/socialmediahub/ui/status/StatusViewModel.kt
@@ -0,0 +1,13 @@
+package com.jatpack.socialmediahub.ui.status
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StatusViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
